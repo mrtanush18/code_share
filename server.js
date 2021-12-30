024815495b3dbc9ef1f7a3de7d82f800da91b5a9
tanush <tanushshetty47@gmail.com>
2021-12-31 02:34:28 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require("express")
+const app = express()
+// folder ejs, public :
+app.set("view engine", "ejs")
+app.use(express.static("public"))
+
+app.use(express.urlencoded({ extended: true }))
+
+const Document = require("./models/Document")
+
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/codeSnipShare", {
+  useUnifiedTopology: true,
+})
+
+app.get("/", (req, res) => {
+  const code = `Welcome to CodeSnipShare!
+
+Use the commands in the top right corner
+to create a new file to share with others.`
+
+  res.render("code-display", { code, language: "plaintext" })
+})
+
+app.get("/new", (req, res) => {
+  res.render("new")
+})
+
+// form textarea is mapped to variable value
+app.post("/save", async (req, res) => {
+  const value = req.body.value
+  try {
+    const document = await Document.create({ value })
+    res.redirect(`/${document.id}`)
+  } catch (e) {
+    res.render("new", { value })
+  }
+})
+
+
+// // generate unique url on pressing save on new route
+app.get("/:id", async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render("code-display", { code: document.value, id })
+  } catch (e) {
+    res.redirect("/")
+  }
+})
+
+app.listen(3000)
